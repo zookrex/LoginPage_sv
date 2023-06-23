930744fb59b9d974bb711254b9655f6c954bc92c
zookrex <arzoosarangal97@gmail.com>
2023-06-23 13:54:31 +0530

    loging in the user

--- src/main/java/com/arzoo/login/controller/Login.java
+++ src/main/java/com/arzoo/login/controller/Login.java
@@ -7,16 +7,20 @@ import com.arzoo.login.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
-@RequestMapping("/login")
+@RequestMapping("/user")
 public class Login {
     @Autowired
     private UserRepo userRepo;
     @Autowired
     private UserService userService;
+
+    @Autowired
+    BCryptPasswordEncoder encoder;
 
 
 
@@ -30,4 +34,17 @@ public class Login {
         userService.registerUser(user);
         return ResponseEntity.ok("User Created withe username : "+user.getUsername());
     }
+    @PostMapping ("/login")
+    public ResponseEntity<String> loginUser(@RequestBody User user){
+        log.info("inside login api");
+        User existingUser= userRepo.findByUsername(user.getUsername());
+        if(existingUser==null)
+            return ResponseEntity.badRequest().body("User is not Registered");
+        if(encoder.matches(user.getPassword(), existingUser.getPassword()))
+            return ResponseEntity.ok().body("Login Successful") ;
+
+        return ResponseEntity.badRequest().body("Chack Credentials");
+    }
+
+
 }
--- src/main/java/com/arzoo/login/service/UserService.java
+++ src/main/java/com/arzoo/login/service/UserService.java
@@ -2,11 +2,13 @@ package com.arzoo.login.service;
 
 import com.arzoo.login.domain.User;
 import com.arzoo.login.repo.UserRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepo userRepo;
@@ -21,7 +23,9 @@ public class UserService {
 
     public void registerUser(User user){
         String encryptPassword=encrypt.encode(user.getPassword());
+       // log.info("-------->"+encrypt.matches(user.getPassword(),encryptPassword));
         user.setPassword(encryptPassword);
         userRepo.save(user);
+
     }
 }
